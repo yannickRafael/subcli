200cd38cc27f94ad44a53abb8d83ffdc97c2b69b
yannickRafael <yannickrafael286@gmail.com>
2025-07-07 22:56:50 +0200

    update readme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name="subcli",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=["click", "requests", "beautifulsoup4", "InquirerPy"],
     entry_points={
